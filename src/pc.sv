6771c96cbcd9d73ef765c530773cfe986a02c076
Raymond Wong <infiwang@pm.me>
2022-05-15 23:28:22 +0800

    pc: init
    maybe split into pcgen and pcreg later?
    
    Signed-off-by: Raymond Wong <infiwang@pm.me>

---
+++ src/pc.sv
@@ -0,0 +1,38 @@
+`include "def.sv"
+
+module pc (
+    input clk,
+    input rst,
+
+    input jmp,
+    input rel,
+    input [31:0] diff,
+    input [31:0] nxt,
+
+    output [31:0] cur
+);
+
+    reg [31:0] r;
+    assign cur = r;
+
+    initial begin
+        r <= 32'b0;
+    end
+
+    always_ff @(posedge clk) begin
+        if(rst) begin
+            r <= 32'b0;
+        end else begin
+            if(jmp) begin
+                if(rel) begin
+                    r <= r + diff;
+                end else begin
+                    r <= nxt;
+                end
+            end else begin
+                r <= r + 4;
+            end
+        end
+    end
+
+endmodule
