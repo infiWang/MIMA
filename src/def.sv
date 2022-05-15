df5a3bd2070d9eee9f28d03ba42c7e982c259167
Raymond Wong <infiwang@pm.me>
2022-05-15 21:41:54 +0800

    def: add funct3 enum for branch opcode instr.
    Signed-off-by: Raymond Wong <infiwang@pm.me>

--- src/def.sv
+++ src/def.sv
@@ -8,3 +8,12 @@ enum bit [2:0] {
     f3OpInt_SLT = 3'b010,
     f3OpInt_SLTU = 3'b011
 } f3OpInt;
+
+enum bit [2:0] {
+    f3Br_EQ = 3'b000,
+    f3Br_NE = 3'b001,
+    f3Br_LT = 3'b100,
+    f3Br_GE = 3'b101,
+    f3Br_LTU = 3'b110,
+    f3Br_GEU = 3'b111
+} f3Br;
