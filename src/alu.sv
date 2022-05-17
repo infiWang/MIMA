3010004540c4bdc39f42f54de4081386b928c77e
Raymond Wong <infiwang@pm.me>
2022-05-17 14:15:29 +0800

    alu: accept and use op signal
    Signed-off-by: Raymond Wong <infiwang@pm.me>

--- src/alu.sv
+++ src/alu.sv
@@ -1,6 +1,7 @@
 `include "def.sv"
 
 module alu (
+    input op,
     input op_imm,
     input [2:0] funct3,
     input [6:0] funct7,
@@ -11,45 +12,49 @@ module alu (
 );
 
     always_comb begin
-        case(funct3)
-            f3OpInt::ADD: begin
-                if(op_imm | funct7 == 7'b0) begin
-                    t = a + b;
-                end else if(funct7 == 7'b0100000) begin
-                    t = a - b;
-                end else begin
-                    t = 32'bx;
+        if(op) begin
+            case(funct3)
+                f3OpInt::ADD: begin
+                    if(op_imm | funct7 == 7'b0) begin
+                        t = a + b;
+                    end else if(funct7 == 7'b0100000) begin
+                        t = a - b;
+                    end else begin
+                        t = 32'bx;
+                    end
                 end
-            end
-            f3OpInt::AND: begin
-                t = a & b;
-            end
-            f3OpInt::OR: begin
-                t = a | b;
-            end
-            f3OpInt::XOR: begin
-                t = a ^ b;
-            end
-            f3OpInt::SL: begin
-                t = a << b;
-            end
-            f3OpInt::SR: begin
-                if(funct7 == 7'b0) begin
-                    t = a >> b;
-                end else if(funct7 == 7'b0100000) begin
-                    t = $signed(a) >>> b;
-                end else begin
-                    t = 32'bx;
+                f3OpInt::AND: begin
+                    t = a & b;
                 end
-            end
-            f3OpInt::SLT: begin
-                t = $signed(a) < $signed(b) ? 32'b1 : 32'b0;
-            end
-            f3OpInt::SLTU: begin
-                t = a < b ? 32'b1 : 32'b0;
-            end
-            default: t = 32'bx;
-        endcase 
+                f3OpInt::OR: begin
+                    t = a | b;
+                end
+                f3OpInt::XOR: begin
+                    t = a ^ b;
+                end
+                f3OpInt::SL: begin
+                    t = a << b;
+                end
+                f3OpInt::SR: begin
+                    if(funct7 == 7'b0) begin
+                        t = a >> b;
+                    end else if(funct7 == 7'b0100000) begin
+                        t = $signed(a) >>> b;
+                    end else begin
+                        t = 32'bx;
+                    end
+                end
+                f3OpInt::SLT: begin
+                    t = $signed(a) < $signed(b) ? 32'b1 : 32'b0;
+                end
+                f3OpInt::SLTU: begin
+                    t = a < b ? 32'b1 : 32'b0;
+                end
+                default: t = 32'bx;
+            endcase
+        end else begin
+            t = 32'bx;
+        end
     end
 
 endmodule
