9bdc788694c55e92b8c8403c44c874f60b440f4b
Raymond Wong <infiwang@pm.me>
2022-05-19 21:50:21 +0800

    mmio: refactor
    Signed-off-by: Raymond Wong <infiwang@pm.me>

--- src/mmio.sv
+++ src/mmio.sv
@@ -1,19 +1,19 @@
-module mmio (input clk,
-             input rst,
-             input load,
-             input store,
-             input [2:0] access,
-             input [31:0] addr,
-             input [31:0] data_in,
-             output reg [31:0] data_out);
-  ram ram0(
-  .clk(clk),
-  .rst(rst),
-  .load(load),
-  .store(store),
-  .access(access),
-  .addr(addr),
-  .data_in(data_in),
-  .data_out(data_out)
-  );
+module mmio (
+    input clk, rst,
+    input load, store,
+    input [2:0] access,
+    input [31:0] addr,
+    input [31:0] data_in,
+    output reg [31:0] data_out
+);
+
+    ram ram0(
+        .clk(clk), .rst(rst),
+        .load(load), .store(store),
+        .access(access),
+        .addr(addr),
+        .data_in(data_in),
+        .data_out(data_out)
+    );
+
 endmodule
